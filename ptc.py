370beae498542269cb5d90136a99c4bc95639e62
12vishalkumar <90981381+12vishalkumar@users.noreply.github.com>
2022-04-20 23:27:33 +0530

    Update ptc.py

--- ptc.py
+++ ptc.py
@@ -1,12 +1,14 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-import re
-T = int(input())
-for i in range(T):
-    Bank = ''.join(input())
-    if(re.match(r'^[456]', Bank) and
-      (re.match(r'([\d]{4}-){3}[\d]{4}$', Bank) or
-      re.match(r'[\d]{16}', Bank)) and not
-      re.search(r'(\d)\1{3,}', Bank.replace('-', ''))):
-        print('Valid')
-    else:
-        print('Invalid')
+def swap_case(s):
+    st =""
+    for i in s:
+        if(i.islower()):
+            st += i.upper()
+        elif(i.isupper()):
+            st += i.lower()
+        else:
+            st += i                
+    return st
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
